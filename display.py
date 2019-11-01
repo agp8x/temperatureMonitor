ffb8e97a05c67444c75055aa903bc4869ab06611
agp8x <git@agp8x.org>
2019-11-01 16:42:01 +0100

    add rom info, use sorted roms

--- display.py
+++ display.py
@@ -24,6 +24,17 @@ def show(i, displays, value):
 	if i < len(displays):
 		displays[i].number(int(value))
 
+def sensor_info(rom):
+	raw = rom
+	litte_int = uniq = int.from_bytes(rom, byteorder='little')
+	big_int = uniq = int.from_bytes(rom, byteorder='big')
+	return (raw, little_int, big_int)
+
+def show_sensors(roms):
+	print("\t".join(["raw", "little", "big"]))
+	for i, rom in enumerate(sorted(roms)):
+		print("\t".join(sensor_info(rom)))
+
 
 def run():
 	ds_pin = Pin(ds18_pin)
@@ -31,12 +42,15 @@ def run():
 
 	roms = ds_sensor.scan()
 	print('Found DS devices: ', roms)
+	sorted_roms = sorted(roms)
+	show_sensors(sorted_roms)
+	
 	displays = setup()
 
 	while True:
 		ds_sensor.convert_temp()
 		time.sleep_ms(750)
-		for i, rom in enumerate(roms):
+		for i, rom in enumerate(sorted_roms):
 			value = ds_sensor.read_temp(rom)
 			print(rom)
 			print(value)
